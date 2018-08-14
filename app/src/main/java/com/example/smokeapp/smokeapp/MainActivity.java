ed9e8699588e9db48df6e8228e155f937be86c24
Jannick Bock <jannick_18@yahoo.com>
2018-08-14 10:50:40 +0200

    fixed onClicks

--- app/src/main/java/com/example/smokeapp/smokeapp/MainActivity.java
+++ app/src/main/java/com/example/smokeapp/smokeapp/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.smokeapp.smokeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class    MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button config = (Button) findViewById(R.id.config);
+        Button statistics = (Button) findViewById(R.id.statistic);
+
+
+        config.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_config);
+            }
+        });
+
+        statistics.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_statistics);
+            }
+        });
+
     }
 }
