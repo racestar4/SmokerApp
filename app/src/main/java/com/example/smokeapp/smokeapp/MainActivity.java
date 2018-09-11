627dbd0523bbf1c29c915866b3ddb142426c9674
Jannick Bock <jannick_18@yahoo.com>
2018-09-11 12:29:38 +0200

    Buttons fixed

--- app/src/main/java/com/example/smokeapp/smokeapp/MainActivity.java
+++ app/src/main/java/com/example/smokeapp/smokeapp/MainActivity.java
@@ -1,57 +1,66 @@
 package com.example.smokeapp.smokeapp;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.PopupMenu;
-import android.widget.PopupWindow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONObject;
 
 public class  MainActivity extends AppCompatActivity {
 
+    User user;
+    JSONObject jsonObject;
+    DataBaseConnection dataBaseConnection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        User user = new User();
-        Button config = (Button) findViewById(R.id.config);
-        Button statistics = (Button) findViewById(R.id.statistic);
-        DataBaseConnection dataBaseConnection = new DataBaseConnection();
-        JSONObject jsonObject;
+
+      //  Button config = findViewById(R.id.config);
+     //   Button statistics =findViewById(R.id.statistic);
+
+        dataBaseConnection = new DataBaseConnection();
+
+        if ( user == null) {
+            user = new User();
+            setContentView(R.layout.activity_config);
+        }else {
+            setContentView(R.layout.activity_main);
+        }
+
         try {
             jsonObject = new JSONObject(dataBaseConnection.doInBackground("userInformation", "bob"));
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.toString());
         }
-        config.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_config);
-            }
-        });
-
-        statistics.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_statistics);
-                Toast.makeText(this, jsonObject.toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
-    public void saveSettings(View view){
-        Toast.makeText(this, "Button1 Clicked!", Toast.LENGTH_SHORT).show();
 
+    // On Click statistics
+    public void statistics (View view){
 
-
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_statistics);
+        Toast.makeText(this, jsonObject.toString(), Toast.LENGTH_SHORT).show();
     }
+    //On Click config
+     public void config (View view) {
+        setContentView(R.layout.activity_config);
+     }
+
+        // On Click save user settings
+        public void saveSettings (View view){
+
+            TextView tv1 =  findViewById(R.id.InputName);
+            TextView tv2 =  findViewById(R.id.InputSize);
+
+            if ( !tv1.getText().toString().equals("")) {
+                user.name = tv1.getText().toString();
+            }
+            if( !tv2.getText().toString().equals("")) {
+                user.height = Integer.parseInt(tv2.getText().toString());
+            }
+            Toast.makeText(this, "Name ist : " + user.name+ " und bin " + user.height + " groß.", Toast.LENGTH_SHORT).show();
+            setContentView(R.layout.activity_main);
+        }
 }
